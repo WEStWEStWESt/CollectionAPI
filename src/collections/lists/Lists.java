3bd831222b55f2f7de7cbdc552484b46dc9c1356
WESt <WESt>
2021-09-14 22:45:08 +0300

    Basic operations to List.

--- src/collections/lists/Lists.java
+++ src/collections/lists/Lists.java
@@ -1,22 +1,43 @@
 package collections.lists;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Lists {
     public static void main(String[] args) {
-        /* Инициализация пустых списков. */
+        /* Инициализация ПУСТЫХ списков. */
+        /* часто используемые*/
         List<String> emptyStrings = new ArrayList<>();/* пустой изменяемый список строк.*/
-        List<?> emptyGenerics = new ArrayList<>();/* нельзя доваить НИЧЕГО !!! ???*/
-        List<? extends Number> emptyNumbers = new ArrayList<>();/*коллекция елементов типа Number и наследников*/
-        List<? super Number> emptySuperNumbers = new ArrayList<>();/*коллекция елементов типа Number и суперклассов(Object) ????*/
-        List rawTypes = new ArrayList();/* коллекция из Objects */
-        var rawTypeReferences = new ArrayList<>();/* коллекция из Objects */
         var stringReferences = new ArrayList<String>();/* пустой изменяемый список строк.*/
         List<String> immutableListStrings = List.of();/* пустой НЕ изменяемый список строк.*/
         List<String> collUtilStrings = Collections.emptyList();/* пустой НЕ изменяемый список строк.*/
         List<String> arraysUtilStrings = Arrays.asList();/* пустой НЕ изменяемый список строк.*/
+
+        /* редко используемые*/
+        List<? extends Number> emptyNumbers = new ArrayList<>();/*коллекция елементов типа Number и наследников*/
+        List<? super Number> emptySuperNumbers = new ArrayList<>();/*коллекция елементов типа Number и суперклассов(Object) ????*/
+        var rawTypeReferences = new ArrayList<>();/* коллекция из Objects */
+        List rawTypes = new ArrayList();/* коллекция из Objects */
+        List<?> emptyGenerics = new ArrayList<>();/* нельзя доваить НИЧЕГО !!! ??? */
+
+        /* Инициализация НЕ ПУСТЫХ списков. */
+        List<String> filledList = List.of("sa", "GG"); // иммутаб
+        List<String> filledArrAsList = Arrays.asList("sa", "asas");// иммутаб
+        List<String> filledSingletonList = Collections.singletonList("sa");// иммутаб
+        var strings = new ArrayList<>(filledList);// создание мутабельной копии коллекции !!!
+        List<String> filledListCopy = List.copyOf(filledList);// иммутаб
+
+        /* Добавление элементов в коллекцию */
+        strings.add("sddddd");
+        strings.addAll(List.of("www", "eee"));
+        strings.add(0, "OOOO");
+        strings.addAll(0, List.of("kkk"));
+        System.out.println("add: " + strings);
+
+        /* ИТЕРАТОР */
+        Iterator<String> iterator = filledList.iterator();
+        while (iterator.hasNext()) {
+            emptyStrings.add(iterator.next());
+        }
+        System.out.println("iterator: " + emptyStrings);
     }
 }
