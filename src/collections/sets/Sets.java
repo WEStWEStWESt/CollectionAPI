b8ea23d955e9fd028019515d70141390f2bec27c
WESt <WESt>
2021-09-16 23:05:46 +0300

    HashSet's work reviewed.

--- src/collections/sets/Sets.java
+++ src/collections/sets/Sets.java
@@ -1,7 +1,34 @@
 package collections.sets;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Sets {
     public static void main(String[] args) {
+        /*
+           SET - неупорядоченное множество уникальных объктов.
+           HashSet строится на основе HashMap. HashMap(key, value) поддерживает уникальность хранимых ключей.
+           Значения при этом могут быть НЕ уникальными.
 
+           Все объекты SETа кладутся в HashMap в виде КЛЮЧА !!! Вместо ЗНАЧЕНИЯ подставляется ПУСТОЙ Object !!!
+
+           Из этого исходят 2 основных применения HashSet:
+           1. HashSet удаляет(автоматически) дублирующиеся объекты, оставляя лишь один экземпляр.
+              NULL  тоже может находится В ЕДИНСТВЕННОМ ЭКЗЕМПЛЯРЕ !
+           2. Проверка на наличие в SETе УЖЕ ИМЕЮЩЕГОСЯ объекта (метод contains()).
+        */
+        List<String> listStrings = new ArrayList<>(List.of("A", "B", "C", "A"));
+        Set<String> setLists = new HashSet<>(listStrings);
+        System.out.println("listStrings: " + listStrings);
+        System.out.println("changed setLists: " + setLists);
+        if (setLists.contains("A")) {
+            setLists.remove("A");
+            setLists.add("F");
+            System.out.println(setLists);
+        } else {
+            System.out.println("Element not found!");
+        }
     }
 }
