c34450676bfc3aa4fd99356be0659013f662ee38
WESt <WESt>
2021-09-28 23:05:33 +0300

    Comments to streams added.

--- src/collections/streams/Streams.java
+++ src/collections/streams/Streams.java
@@ -2,6 +2,7 @@ package collections.streams;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -24,7 +25,7 @@ public class Streams {
               Тут посредством работы лямда-выражения происходит добавление строки со значением "А",
               к КАЖДОМУ ЭЛЕМЕНТУ СТРИМА.
            4. Метод sorted() сортирует Stream-объекты исходя из естественного порядка сортировки внутри них.
-
+              Объекты ДОЛЖНЫ БЫТЬ Comparable !!!
            5. Вызов ТЕРМИНАЛЬНОГО МЕТОДА collect().
               Для определения порядка и характера производимых промежуточных операций, терминальный метод использует
               метаданные объекта stream, из которого он вызывается. После выполнения всех операций, объекты стрима
@@ -39,16 +40,32 @@ public class Streams {
         /*3*/
         stream = stream.map(string -> string + "A");
         /*4*/
-        stream = stream.sorted();// ??????????????????????????????????????????????????????????
+        stream = stream.sorted();
         /*5*/
         List<String> strings = stream.collect(Collectors.toList());
-        System.out.println(strings);
-        System.out.println(Arrays.toString(arr));
 
-        System.out.println(strings.stream()
+        System.out.println("sorted stream: " + strings);
+        System.out.println("initial array: " + Arrays.toString(arr));
+
+        System.out.println("stream count: " + strings.stream()
                 .filter("a1A"::equals)
                 .map(s -> s.substring(0, s.lastIndexOf("A")))
-                .peek(System.out::println)
                 .count());
+
+        Map<Character, List<String>> collect = strings.stream()
+                .collect(Collectors.groupingBy(s -> s.charAt(1)));
+        System.out.println("groupingBy first character: " + collect);
+
+        System.out.println("any elements startsWith \"a\": " + strings.stream()
+                .anyMatch(s -> s.startsWith("a")));
+
+        System.out.println("filtering with Optional, if exists - remove the last \"A\": " + strings.stream()
+                .filter(s -> s.endsWith("A"))
+                .findAny()
+                .map(s -> s.substring(0, s.lastIndexOf("A")))
+                .orElse("unknown"));
+
+        System.out.println("PRINTING STREAM USING FOREACH: ");
+        strings.forEach(System.out::println);
     }
 }
